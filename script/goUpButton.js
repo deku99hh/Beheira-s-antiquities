b67e13faf3bba3ec4f849d1fbb173cd81a7eca99
deku99hh <omar.rashed1000@gmail.com>
2025-12-11 14:15:33 +0200

    english btn

--- script/goUpButton.js
+++ script/goUpButton.js
@@ -93,3 +93,25 @@ function RashedTheMan(){
   },3000)
 
 };
+
+
+
+const arabicBTN = document.querySelector('.arabic');
+const englishBTN = document.querySelector('.english');
+const body = document.body;
+
+addEventListener('DOMContentLoaded',()=>{
+
+  body.classList.remove('arabic', 'english');
+  body.classList.add('arabic');
+
+
+  arabicBTN.addEventListener('click', () => {
+    body.classList.remove('arabic', 'english');
+    body.classList.add('arabic');
+  });
+  englishBTN.addEventListener('click', () => {
+    body.classList.remove('arabic', 'english');
+    body.classList.add('english');
+  });
+});
